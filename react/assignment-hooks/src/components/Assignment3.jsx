dc19ed77d208dace577eb0e026aad83b0a0cd0d5
Aditya Surana <suranaaditya3@gmail.com>
2024-07-09 19:17:15 +0530

    useMemo assignment

--- react/assignment-hooks/src/components/Assignment3.jsx
+++ react/assignment-hooks/src/components/Assignment3.jsx
@@ -1,8 +1,44 @@
+import { useMemo, useState } from "react"
 
 
 export function Assignment3(){
+    const [items, setItems] = useState([
+        {'name':'Choclates', 'value': 10},
+        {'name':'Chips', 'value': 7},
+        {'name':'Onion', 'value': 101},
+        {'name':'Tomato', 'value': 121},
+    ])
+    // const totalValue = useMemo(()=>{
+        // var totalValue = 0;
+        // for(let i=0;i<items.length;i++){
+        //     totalValue += items[i].value
+        // }
+        // return totalValue
+        const totalValue1 = useMemo(() => {
+            const totalValue = items.reduce((acc, item) => {
+                return acc += item.value
+            },0)
+            return totalValue
+        })
+        
+
+          
+        // items.reduce((acc,i) => returnacc+=i.value)
+    // }, [items])
+    
+    
     return(
         <>
+        <div>
+            <ul>
+            {items.map((i, index) => 
+            <li key={index}>{i.name} - Price: ${i.value}</li>
+            )}
+            </ul>
+            
+            <p>Tota price: {totalValue1}</p>
+        </div>
+
         </>
     )
 } 
