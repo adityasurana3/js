dd9aecfa907639ef553bbdc722c38a8f4e8e4646
Aditya Surana <aditya.surana@proshore.eu>
2024-07-18 16:50:55 +0545

    react-router

--- react/hooks/src/App.jsx
+++ react/hooks/src/App.jsx
@@ -1,54 +1,34 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './App.css'
 import axios from "axios";
 
 function App() {
-  const [id, setId] = useState(1)
-  function getId(i){
-    setId(i)
+  const [counter, setCounter] = useState(0)
+  const [inputValue, setInputValue] = useState(1)
+
+  let count = useMemo(()=>{
+    let finalCount = 0
+    for (let i = 1; i <= inputValue; i++) {
+      finalCount += i;
+    }
+    return finalCount;
+  },[inputValue])
+
+  
+
+  function calculateSum(){
+    setCounter(counter+1)
   }
   return(
-    <div>
-      <button onClick={function(){
-        getId(1)
-      }}>1</button>
-      <button onClick={function(){
-        getId(2)
-      }}>2</button>
-      <button onClick={function(){
-        getId(3)
-      }}>3</button>
-      <button onClick={function(){
-        getId(4)
-      }}>4</button>
-      <Todo id={id}></Todo>
-    </div>
+  <>
+    <input type="text" onChange={function(e){
+      setInputValue(e.target.value)
+    }}/> <br />
+    <h1>Sum is 1 to {inputValue} is {count}</h1>
+    <button onClick={calculateSum}>Counter ({counter})</button>
+  </>
   )
 }
 
-
-function Button({num}){
-  return(
-    <button >{num}</button>
-  )
-}
-
-function Todo({id}){
-  const [todo, setTodo] = useState({})
-  useEffect(()=>{
-    axios.get("https://sum-server.100xdevs.com/todo?id="+id)
-    .then(function(response){
-      console.log(response.data.todo)
-      setTodo(response.data.todo)
-    })
-  },[id])
-  return(
-    <div>
-      {}
-      <h1>{todo.title}</h1>
-      {todo.description}
-    </div>
-  )
-}
 
 export default App;
